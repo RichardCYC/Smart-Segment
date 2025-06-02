f9c3ec0883bbe137ac2a31be70e2e0301a09b31a
Richard Cheng <richardchengyc@gmail.com>
2025-06-02 17:14:59 +0800

    Move gtag to head

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -8,6 +8,24 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
         />
+        {process.env.NEXT_PUBLIC_GA_ID && (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+                `,
+              }}
+            />
+          </>
+        )}
       </Head>
       <body>
         <Main />
